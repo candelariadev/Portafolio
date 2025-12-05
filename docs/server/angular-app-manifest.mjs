fca01ec234eca1c9fd5c4e32cebf21ecce08a992
candelariadev <candelariacabreradev@gmail.com>
2025-12-05 16:34:07 -0600

    deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 2226, hash: 'f073e86e9ca3118bb884499421807db9f6dd81de85d1fdc416538635e56a7c49', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1586, hash: '2365bd33bd99a4264c823255b8dd03b49789e49623a6a8b3cf14e80533ac750c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2226, hash: '7e5ff30b98703f8ab86e9f420e7e08641367af7dc93cea505e0dd50cdc4f0062', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1586, hash: 'e9875be8645750e2b5bb539477b4064b454e5c19f31898ade3b8b1ded2477b25', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-UJYRXEQK.css': {size: 96399, hash: 'QolypY7uDfo', text: () => import('./assets-chunks/styles-UJYRXEQK_css.mjs').then(m => m.default)}
   },
 };
