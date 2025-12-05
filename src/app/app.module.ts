da4be07dd459931688be4e110ef86d639373cec3
candelariadev <candelariacabreradev@gmail.com>
2025-12-05 10:41:20 -0600

    image home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { AboutComponent } from './layouts/about/about.component';
 import { SkillsComponent } from './layouts/skills/skills.component';
 import { MyservicesComponent } from './layouts/myservices/myservices.component';
@@ -13,12 +12,12 @@ import { CircularProgressBarComponent } from './components/circular-progress-bar
 import { ProjectsComponent } from './layouts/projects/projects.component';
 import { ContactmeComponent } from './layouts/contactme/contactme.component';
 import { ProjectsGalleryComponent } from './views/projects-gallery/projects-gallery.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent,
     AboutComponent,
     SkillsComponent,
     MyservicesComponent,
@@ -26,11 +25,14 @@ import { ProjectsGalleryComponent } from './views/projects-gallery/projects-gall
     CircularProgressBarComponent,
     ProjectsComponent,
     ContactmeComponent,
-    ProjectsGalleryComponent
+    ProjectsGalleryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarComponent
+
+
   ],
   providers: [
     provideClientHydration()
